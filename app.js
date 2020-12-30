dba5ebfb241bee469d006d4bdfaf9698fa0f50b4
Phos23 <phos23@gmail.com>
2020-12-30 21:06:01 +0000

    add cors settings

--- app.js
+++ app.js
@@ -3,24 +3,10 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-    console.log('request received');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
-
-app.use((req, res, next) => {
-    res.status(201);
-    next();
-});
-
-app.use((req, res, next) => {
-    res.json({
-        message: 'your request was successful'
-    });
-    next();
-});
-
-app.use((req, res, next) => {
-    console.log('request sent successfully')
-})
 
 module.exports = app;
