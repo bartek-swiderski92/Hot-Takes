37dd19eafd94d0fac6b0408e84daee7d9ad88f52
Phos23 <phos23@gmail.com>
2020-12-30 22:24:14 +0000

    add creating new users

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const userRoutes = require('./routes/user');
 
 const app = express();
 
@@ -22,5 +23,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
+
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
