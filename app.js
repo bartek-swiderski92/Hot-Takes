277121f2db70c8c5e5a370e762dda8a1bd6cc87b
Phos23 <phos23@gmail.com>
2020-12-30 21:33:36 +0000

    setup mongoose

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
+//MONGODB PW: 9wAytpdXHGTQ94e
+//MONGODB CONNECTION: mongodb+srv://Bartosz:<password>@cluster0.edbqm.mongodb.net/<dbname>?retryWrites=true&w=majority
+
 const express = require('express');
+const mongoose = require('mongoose');
+
 
 const app = express();
+
+mongoose.connect('mongodb+srv://Bartosz:9wAytpdXHGTQ94e@cluster0.edbqm.mongodb.net/<dbname>?retryWrites=true&w=majority')
+    .then(() => {
+        console.log('Successfully connected to MongoDB Atlas!');
+    })
+    .catch((error) => {
+        console.log('Unable to connect to MongoDB Atlas!');
+        console.error(error)
+    });
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
