2162a2522c800a4c5dbd30f8357c8bd9527e5c57
Phos23 <phos23@gmail.com>
2020-12-30 19:29:55 +0000

    add test middleware

--- app.js
+++ app.js
@@ -2,4 +2,25 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+    console.log('request received');
+    next();
+});
+
+app.use((req, res, next) => {
+    res.status(201);
+    next();
+});
+
+app.use((req, res, next) => {
+    res.json({
+        message: 'your request was successful'
+    });
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('request sent successfully')
+})
+
 module.exports = app;
