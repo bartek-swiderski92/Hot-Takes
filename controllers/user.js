ff6a0b37ba95c10f3f918a2175fab67837565dfa
Phos23 <phos23@gmail.com>
2020-12-31 15:11:00 +0000

    add login function

--- controllers/user.js
+++ controllers/user.js
@@ -25,5 +25,35 @@ exports.signup = (req, res, next) => {
 };
 
 exports.login = (req, res, next) => {
-
+    User.findOne({
+        email: req.body.email
+    }).then(
+        (user) => {
+            if (!user) {
+                return res.status(401).json({
+                    error: new Error('User not found!')
+                });
+            }
+            bcrypt.compare(req.body.password, user.password).then(
+                (valid) => {
+                    if (!valid) {
+                        return res.status(401).json({
+                            error: new Error('Incorrect password!')
+                        });
+                    }
+                    res.status(200).json({
+                        userId: user._id,
+                        token: 'token'
+                    });
+                }
+            ).catch((error) => {
+                res.status(500).json({
+                    error: error
+                });
+            });
+        }).catch((error) => {
+        res.status(500).json({
+            error: error
+        });
+    });
 };
