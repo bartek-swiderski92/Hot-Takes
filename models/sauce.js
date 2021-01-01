2a25cb9614a5f9d99619e1d6627f0ffc087e688c
Phos23 <phos23@gmail.com>
2021-01-01 17:56:10 +0000

    fix manufacturer typo

--- models/sauce.js
+++ models/sauce.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const sauceSchema = mongoose.Schema({
     userId: { type: String, required: true },
     name: { type: String, required: true },
-    manufacter: { type: String, required: true },
+    manufacturer: { type: String, required: true },
     description: { type: String, required: true },
     mainPepper: { type: String, required: true },
     imageUrl: { type: String, required: true },
