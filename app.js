2c226825896c4b769ae6e8b5d7056425c66a3d1d
Phos23 <phos23@gmail.com>
2021-01-01 17:40:06 +0000

    optimized app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
+const saucesRoutes = require('./routes/sauces')
 const userRoutes = require('./routes/user');
-
-const Sauce = require('./models/sauce');
 
 const app = express();
 
@@ -16,64 +16,9 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-app.post('/api/sauces', (req, res, next) => {
-    const sauce = new Sauce({
-        userId: req.body.userId,
-        name: req.body.name,
-        manufacturer: req.body.manufacturer,
-        description: req.body.description,
-        mainPepper: req.body.mainPepper,
-        imageUrl: req.body.imageUrl,
-        heat: req.body.heat,
-        likes: req.body.likes,
-        dislikes: req.body.dislikes,
-        usersLiked: req.body.usersLiked,
-        usersDisliked: req.body.usersDisliked,
-    });
-    sauce.save().then(
-        () => {
-            res.status(201).json({
-                message: 'Post saved successfully!'
-            })
-        }).catch((error) => {
-        res.status(400).json({
-            error: error
-        })
-    })
-});
+app.use('/images/', express.static(path.join(__dirname, 'images')));
 
-app.use('/api/sauces', (req, res, next) => {
-    const sauces = [{
-        _id: '123',
-        userId: '1234',
-        name: 'Hot sauce',
-        manufacter: 'helmans',
-        description: 'hottest sauce avaliable on the market',
-        mainPepper: 'chilli',
-        imageUrl: 'https://www.chileseeds.co.uk/wp-content/uploads/2016/10/SweetChilliHotSauces-600x600.jpg',
-        heat: 7,
-        likes: 234,
-        dislikes: 20,
-        userLiked: [1234, 12345]
-
-    }, {
-        _id: '12',
-        userId: '12345',
-        name: 'Hotttest sauce',
-        manufacter: 'helmans',
-        description: 'hottest sauce avaliable on the market',
-        mainPepper: 'chilli',
-        imageUrl: 'https://www.chileseeds.co.uk/wp-content/uploads/2016/10/SweetChilliHotSauces-600x600.jpg',
-        heat: 10,
-        likes: 2,
-        dislikes: 1,
-        userLiked: [1234, 12345],
-        usersDisliked: [123456]
-    }];
-    res.status(200).json(sauces)
-});
-
-
+app.use('/api/sauces', saucesRoutes);
 app.use('/api/auth', userRoutes);
 
 module.exports = app;
